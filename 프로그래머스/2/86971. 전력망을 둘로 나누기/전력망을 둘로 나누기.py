de3533cf69ece1304d70d5c8323361e330bbc2f2
myoungjinGo <coke_mania@naver.com>
2024-05-07 22:37:29 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 238.45 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -2,10 +2,9 @@ def solution(n, wires):
     answer = n
     for severed_wires in (wires[i+1:] + wires[:i] for i in range(len(wires))):
         a_group = set(severed_wires[0])
-        [a_group.update(severed_wire) for _ in severed_wires for severed_wire in severed_wires if set(severed_wire) & a_group]
+        for _ in severed_wires:
+            for severed_wire in severed_wires:
+                if set(severed_wire) & a_group:
+                    a_group.update(severed_wire)
         answer =  min(answer, abs(2 * len(a_group) - n))
     return answer
-
-
-
-
