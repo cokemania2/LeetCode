b0a16ab3f6dc6a14eb4cf274713f0a4283536594
myoungjinGo <coke_mania@naver.com>
2025-01-13 15:25:20 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.38 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -0,0 +1,15 @@
+def find_column(ext):
+    if ext == "code":
+        return 0
+    elif ext == "date":
+        return 1
+    elif ext == "maximum":
+        return 2
+    return 3
+
+
+def solution(data, ext, val_ext, sort_by):
+    answer = [[]]
+    filtered_data = list(filter(lambda x: x[find_column(ext)] < val_ext, data))
+    sorted_data = sorted(filtered_data, key = lambda x: x[find_column(sort_by)])
+    return sorted_data
