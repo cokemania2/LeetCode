63db366dab7da0b6a80dc91363860ab687dead59
myoungjinGo <coke_mania@naver.com>
2025-02-18 00:03:14 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,9 +1,3 @@
 def solution(n):
-    x = 1
-    before = bin(n)[2:].count('1')
-    while True:
-        if bin(n + x)[2:].count('1') == before:
-            break
-        x += 1
-    
-    return n + x
+    answer = 0
+    return answer
