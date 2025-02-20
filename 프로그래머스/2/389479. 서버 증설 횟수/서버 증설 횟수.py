3b44f66c2515d9634e2f7cca3384740075ea0433
myoungjinGo <coke_mania@naver.com>
2025-02-20 19:56:35 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.py
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.py
@@ -1,15 +1,11 @@
-# 뒤집고, 슬라이딩 윈도우 방식으로 
-
 def solution(players, m, k):
     answer = 0
     
     server_capacity = [m for _ in range(24)]
-    x  = [0 for _ in range(24)]
     for i in range(24):
         if server_capacity[i] <= players[i]:
             server_needed = (players[i] - server_capacity[i]) // m + 1
             answer += server_needed
-            x[i] = server_needed
             
             for j in range(i, min(i+k, 24)):
                 server_capacity[j] += (server_needed) * m
